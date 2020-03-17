792dc01b2840f2c87d4c441715dc877040d6089e
Berty <hatebreed1991@mail.ru>
2020-03-18 00:02:00 +0700

    feat: add solution for 2 task

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,45 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    
+    NSMutableArray *test = [NSMutableArray arrayWithArray:array];
+    NSMutableArray *currentSumm = [[NSMutableArray alloc]init];
+    NSMutableArray *final = [[NSMutableArray alloc]init];
+    
+    int summ = 0;
+    
+    for(int i = 0; i < [test count]; i++){
+        summ = summ+[test[i] intValue];
+    }
+    
+    for(int i = 0; i < [test count]; i++) {
+        
+        int summWithoutCurrentElement = summ-[test[i] intValue];
+        NSNumber *nsNumberWrap = [NSNumber numberWithInt:(summWithoutCurrentElement)];
+        [currentSumm addObject:(nsNumberWrap)];
+    }
+    
+    int max = [currentSumm[0] intValue];
+    int min = [currentSumm[0] intValue];
+    
+    for (int i = 0; i < [currentSumm count]; i++) {
+        if (max<[currentSumm[i] intValue]) {
+            max = [currentSumm[i] intValue];
+            NSLog(@"max is %d", max);
+        }
+        if (min>[currentSumm[i] intValue]) {
+            min = [currentSumm[i] intValue];
+            NSLog(@"min is %d", min);
+        }
+    }
+    
+    NSNumber *maxFinal = [NSNumber numberWithInt:(max)];
+    NSNumber *minFinal = [NSNumber numberWithInt:(min)];
+    
+    [final addObject:(minFinal)];
+    [final addObject:(maxFinal)];
+    
+    return final;
 }
 
 @end
