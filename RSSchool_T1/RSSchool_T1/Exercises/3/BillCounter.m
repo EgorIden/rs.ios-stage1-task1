0794a5a704309aa90bc80deae68bc702680e5808
Berty <hatebreed1991@mail.ru>
2020-03-18 00:03:53 +0700

    feat: add solution for 3 task

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,25 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    
+    NSMutableArray *test = [NSMutableArray arrayWithArray:(bill)];
+   
+    [test removeObjectAtIndex:(index)];
+    
+    int summ = 0;
+    
+    for(int i = 0; i < [test count]; i++){
+        summ = summ+[test[i] intValue];
+    }
+    
+    int calculatedSumm = summ/2;
+    
+    if (calculatedSumm==[sum intValue]) {
+        return @"Bon Appetit";
+    }else{
+        NSString *difference = [NSString stringWithFormat:@"%i",[sum intValue]-calculatedSumm];
+        return difference;
+    }
 }
 
 @end
