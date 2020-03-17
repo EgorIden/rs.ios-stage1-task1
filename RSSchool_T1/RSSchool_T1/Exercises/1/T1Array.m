26aa5757b748aa3d58b9db77cdaddc4299fd15ea
Berty <hatebreed1991@mail.ru>
2020-03-17 23:56:36 +0700

    feat: add solution for 1 task

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,27 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    
+    NSMutableArray *test = [NSMutableArray arrayWithArray:sadArray];
+    
+    if (test.count==0 || test.count<3) {
+        return test;
+    }
+    
+    int i = 1;
+    
+    while (i<test.count-1){
+        if (([test[i-1] integerValue]+[test[i+1] integerValue])<[test[i] integerValue]) {
+            [test removeObjectAtIndex:i];
+            if ((i-1)!=0) {
+                i--;
+            }
+        }else{
+            i++;
+        }
+    }
+    
+    return test;
 }
 
 @end
