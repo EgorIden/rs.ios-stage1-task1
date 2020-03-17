8b99d284c9a2b74bb469d36eb55d5bf6d1e3b75f
Berty <hatebreed1991@mail.ru>
2020-03-18 00:22:25 +0700

    feat: add solution for 4 task

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,31 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    
+    NSString *searchText = string;
+
+    NSError *error = NULL;
+    
+    NSArray *pattern = @[@"\\<(.*?)\\>",@"\\[(.*?)\\]",@"\\((.*?)\\)",@"\\(\\[(.*?)\\]\\)"];
+    
+    NSMutableArray *matches = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i<pattern.count; i++) {
+        
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern[i]
+                                                            options:NSRegularExpressionCaseInsensitive
+                                                            error:&error];
+        
+        NSArray *myArray = [regex matchesInString:searchText options:0 range:NSMakeRange(0, [searchText length])];
+
+        for (NSTextCheckingResult *match in myArray) {
+             NSRange matchRange = [match rangeAtIndex:1];
+             [matches addObject:[searchText substringWithRange:matchRange]];
+             NSLog(@"matches %@", [matches lastObject]);
+        }
+        
+    }
+    return matches;
 }
 
 @end
